ab559c568ae0c34a1acc12690fd0460df44250a3
Santiago Arango Gutierrez <santiago9606@gmail.com>
2022-04-24 11:27:50 -0500

    Add useState for sort task completed

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,24 @@
 import * as React from 'react'
-import {Image, StyleSheet, Text, View} from "react-native";
+import {Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import TodoList from "../components/TodoList";
 import {todosData} from "../data/todos";
 import {Feather} from "@expo/vector-icons";
 
 
 export default function Home() {
+
+    const [localData, setLocalData] = React.useState(
+        todosData.sort((itemOne,itemTwo) => {return itemOne.isCompleted - itemTwo.isCompleted})
+    )
+
     return (
         <View style={styles.container}>
             <Image
                 source={{uri: 'https://media-exp1.licdn.com/dms/image/C5603AQG_q5BWShIb_A/profile-displayphoto-shrink_200_200/0/1611847931174?e=1656547200&v=beta&t=C3OdfY3H4eY3g-72ZLvJannpRiJlhbd9uh6FfoUHVGQ'}}
                 style={styles.pic}/>
-            <Text style={styles.title}><Feather name="chevron-right" size={24} color="black" /> Today</Text>
+            <Text style={styles.title}><Feather name="chevron-right" size={24} color="black"/> Today</Text>
             <TodoList todosData={todosData.filter(item => item.isToday)}/>
-            <Text style={styles.title}><Feather name="chevron-right" size={24} color="black" /> Tomorrow</Text>
+            <Text style={styles.title}><Feather name="chevron-right" size={24} color="black"/> Tomorrow</Text>
             <TodoList todosData={todosData.filter(item => !item.isToday)}/>
         </View>
     );
